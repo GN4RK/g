6d729a77f267c270c1f73939303b6293c4025cfb
Yoann LEONARD <yoann.leonard@gmail.com>
2023-12-07 21:53:13 +0100

    added status, state and moveset

--- src/model/Entity.php
+++ src/model/Entity.php
@@ -5,12 +5,41 @@ namespace YoannLeonard\G\model;
 class Entity
 {
     private $entityName;
+    private $state;
+    private $status;
     private $health;
     private $attack;
     private $defense;
 
+    public const MOVESET = [
+        'attack' => 0.5,
+        'defense' => 0.4,
+        'flee' => 0.1
+    ];
+
+    public const STATE = [
+        'ready' => 1,
+        'attacking' => 2,
+        'defending' => 3,
+        'fleeing' => 4
+    ];
+
+    public const STATUS = [
+        'normal' => 1,
+        'poisoned' => 2,
+        'burned' => 3,
+        'paralyzed' => 4,
+        'asleep' => 5,
+        'frozen' => 6,
+        'confused' => 7,
+        'flee' => 8,
+        'dead' => 9
+    ];
+
     public function __construct(int $health, int $attack, int $defense)
     {
+        $this->status = 'normal';
+        $this->state = 'ready';
         $this->health = $health;
         $this->attack = $attack;
         $this->defense = $defense;
@@ -58,6 +87,34 @@ class Entity
     public function setDefense(int $defense): void
     {
         $this->defense = $defense;
+    }
+
+    public function setStatus(string $status): void
+    {
+        $this->status = $status;
+    }
+
+    public function getStatus(): string
+    {
+        if ($this->status === null) {
+            return 'normal';
+        }
+        return $this->status;
+    }
+
+    public function chooseAction(): string
+    {
+        $randomNumber = mt_rand() / mt_getrandmax();
+
+        $cumulativeProbability = 0;
+        foreach ($this::MOVESET as $move => $probability) {
+            $cumulativeProbability += $probability;
+            if ($randomNumber <= $cumulativeProbability) {
+                return $move;
+            }
+        }
+
+        return array_key_first($this::MOVESET);
     }
 
 
