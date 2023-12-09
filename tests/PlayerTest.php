1cf03cd4c6e3d5333f2b3f04c89228163a2a126c
Yoann LEONARD <yoann.leonard@gmail.com>
2023-12-09 22:57:43 +0100

    unit tests

---
+++ tests/PlayerTest.php
@@ -0,0 +1,79 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use YoannLeonard\G\model\Entity\Player;
+use YoannLeonard\G\model\Move\Defense;
+
+final class PlayerTest extends TestCase
+{
+    public function testPlayer()
+    {
+        $player = new Player("PlayerTest", 100, 100, 100);
+        $this->assertEquals("PlayerTest", $player->getName());
+        $this->assertEquals(100, $player->getmaxHealth());
+        $this->assertEquals(100, $player->getBaseAttack());
+        $this->assertEquals(100, $player->getBaseDefense());
+        $this->assertEquals(100, $player->getHealth());
+        $this->assertEquals(100, $player->getAttack());
+        $this->assertEquals(100, $player->getDefense());
+        $this->assertEquals(0, $player->getExperience());
+        $this->assertEquals(10, $player->getGold());
+        $this->assertEquals(1, $player->getLevel());
+
+        $player->setName("PlayerTest2");
+        $this->assertEquals("PlayerTest2", $player->getName());
+
+        $player->setLevel(2);
+        $this->assertEquals(2, $player->getLevel());
+
+        $player->setExperience(100);
+        $this->assertEquals(100, $player->getExperience());
+
+        $player->setGold(100);
+        $this->assertEquals(100, $player->getGold());
+
+        $player->setHealth(50);
+        $this->assertEquals(50, $player->getHealth());
+        $this->assertEquals(100, $player->getmaxHealth());
+
+        $player->setAttack(50);
+        $this->assertEquals(50, $player->getAttack());
+        $this->assertEquals(100, $player->getBaseAttack());
+
+        $player->setDefense(50);
+        $this->assertEquals(50, $player->getDefense());
+        $this->assertEquals(100, $player->getBaseDefense());
+
+        $player->setmaxHealth(200);
+        $this->assertEquals(200, $player->getmaxHealth());
+        $this->assertEquals(50, $player->getHealth());
+
+        $player->setBaseAttack(200);
+        $this->assertEquals(200, $player->getBaseAttack());
+        $this->assertEquals(50, $player->getAttack());
+
+        $player->setBaseDefense(200);
+        $this->assertEquals(200, $player->getBaseDefense());
+        $this->assertEquals(50, $player->getDefense());
+
+        $this->assertEquals(true, $player->isAlive());
+        $player->setHealth(0);
+        $this->assertEquals(false, $player->isAlive());
+
+        $player->setHealth(-1);
+        $this->assertEquals(false, $player->isAlive());
+    }
+
+    public function testMoves()
+    {
+        $player = new Player("PlayerTest", 100, 100, 100);
+        $player->setMove(new Defense($player));
+        $this->assertEquals("defense", $player->getMove()->getName());
+        $player->chooseActionFromString("attack");
+        $this->assertEquals("attack", $player->getMove()->getName());
+        $player->chooseActionFromString("flee");
+        $this->assertEquals("flee", $player->getMove()->getName());
+        $player->chooseActionFromString("defense");
+        $this->assertEquals("defense", $player->getMove()->getName());
+
+    }
+}
---
+++ tests/RatTest.php
@@ -0,0 +1,54 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use YoannLeonard\G\model\Entity\Rat;
+
+final class RatTest extends TestCase
+{
+    public function testRat()
+    {
+        $rat = new Rat();
+        $this->assertEquals("Rat", $rat->getEntityName());
+        $this->assertEquals(10, $rat->getmaxHealth());
+        $this->assertEquals(5, $rat->getBaseAttack());
+        $this->assertEquals(5, $rat->getBaseDefense());
+        $this->assertEquals(10, $rat->getHealth());
+        $this->assertEquals(5, $rat->getAttack());
+        $this->assertEquals(5, $rat->getDefense());
+        $this->assertEquals(5, $rat->getExperience());
+        $this->assertEquals(0, $rat->getGold());
+
+        $rat->setExperience(100);
+        $this->assertEquals(100, $rat->getExperience());
+
+        $rat->setGold(100);
+        $this->assertEquals(100, $rat->getGold());
+
+        $rat->setHealth(50);
+        $this->assertEquals(50, $rat->getHealth());
+        $this->assertEquals(10, $rat->getmaxHealth());
+
+        $rat->setAttack(50);
+        $this->assertEquals(50, $rat->getAttack());
+        $this->assertEquals(5, $rat->getBaseAttack());
+
+        $rat->setDefense(50);
+        $this->assertEquals(50, $rat->getDefense());
+        $this->assertEquals(5, $rat->getBaseDefense());
+
+        $rat->setmaxHealth(200);
+        $this->assertEquals(200, $rat->getmaxHealth());
+        $this->assertEquals(50, $rat->getHealth());
+
+        $rat->setBaseAttack(200);
+        $this->assertEquals(200, $rat->getBaseAttack());
+        $this->assertEquals(50, $rat->getAttack());
+
+        $rat->setBaseDefense(200);
+        $this->assertEquals(200, $rat->getBaseDefense());
+        $this->assertEquals(50, $rat->getDefense());
+
+        $this->assertEquals(true, $rat->isAlive());
+        $rat->setHealth(0);
+        $this->assertEquals(false, $rat->isAlive());
+    }
+}
