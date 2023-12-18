82cca7c84ef0cc335b52dddc74993a8c709f012e
yoann.leonard <yoann.leonard@gmail.com>
2023-12-18 17:14:35 +0100

    add itemName property

--- src/Model/Item.php
+++ src/Model/Item.php
@@ -4,6 +4,7 @@ namespace YoannLeonard\G\Model;
 
 abstract class Item
 {
+    private string $itemName;
     private string $name;
     private int $price;
 
@@ -11,6 +12,15 @@ abstract class Item
     {
         $this->setName($name);
         $this->setPrice($price);
+
+        $itemClassPath = get_class($this);
+        $nameParts = explode('\\', $itemClassPath);
+        $this->setItemName(end($nameParts));
+    }
+
+    public function getItemName(): string
+    {
+        return $this->itemName;
     }
 
     public function getName(): string
@@ -21,6 +31,11 @@ abstract class Item
     public function getPrice(): int
     {
         return $this->price;
+    }
+
+    public function setItemName(string $itemName): void
+    {
+        $this->itemName = $itemName;
     }
 
     public function setName(string $name): void
@@ -38,5 +53,9 @@ abstract class Item
         return $this->getName();
     }
 
+    /**
+     * @param Entity $entity The user
+     * @return string[]
+     */
     abstract public function use(Entity $entity): void;
 }
